588a63752e1f0fc6270a79ae37b3a0d8ea70bef6
Hang Xian <hsxian@ucsc.edu>
2024-07-05 10:47:39 -0700

    updated exercise 4 to use useeffect with state

--- src/components/Exercise4.jsx
+++ src/components/Exercise4.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Typography, Button, TextField } from '@mui/material';
 import '../App.css';
 import { useState, useEffect } from 'react';
 
@@ -13,18 +13,67 @@ const fetchUsers = (setUsers) => {
     });
 }
 
+const fetchUser = (id, setUser) => {
+  fetch(`http://localhost:3000/users/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setUser(data)
+    })
+    .catch((err) => {
+      console.log(err);
+      setUser(undefined);
+    });
+}
+
 export default function Exercise4() {
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(undefined);
+  const [id, setId] = useState([-1]);
+  const [input, setInput] = useState('');
 
+  // Fetch users at render
   useEffect(() => {
     fetchUsers(setUsers);
   }, []);
+
+  // Fetch whenever id changes
+  useEffect(() => {
+    fetchUser(id, setUser);
+  }, [id]);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleId = () => {
+    if (input === '') {
+      alert('Input is required');
+    } else {
+      setId(input);
+      setInput('');
+    }
+  }
 
   return(
     <div className='exercise4'>
       {users.map((user, key) => {
         return <Typography variant='h5' key={key}>{user.name}, {user.email}, {user.role}</Typography>
       })}
+      <div className='display'>
+        {(user) && <Typography variant='h2'>Hello, {user.name}!</Typography>}
+      </div>
+      <div className='input'>
+        <TextField
+          required
+          id='id-required'
+          label='Required'
+          placeholder='Enter ID'
+          name='userInput'
+          value = {input}
+          onChange={handleInput}
+          />
+        <Button sx={{marginLeft: '1rem'}} variant="contained" className='change-btn' onClick={handleId}>Search</Button>
+      </div>
     </div>
   )
 }
